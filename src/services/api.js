3d8ced84927f3a0573273d0285b2cfbfeed23163
Milena <milenahikari11@gmail.com>
2020-06-14 10:09:44 -0300

    Ajuste no token

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = 'BQDy3813qEWxbuWnNyjvLLK3hEt71VSspSF5Gk7rodZH7gx3BMIQqeE_uiZaoZSCfySLAccadHTmAYbHxaJ4wXpecUHUfjXopCV5fk1DlQpptkCL5lAi9GvKQEYr83uXRn3pws-c0RAS8CkbglZ66Qi2s9hvB7GUbHrVWd9g-wUFEpE71dFKLSmdUCGSyqzVIz09hpK9YHqOJRe5L1-Vcyf0GwBJ8TG91gQYw_rNevxnW2QePUX1p0cTAyXaXh3tHOJe9CzOY9YghhU';
+const token = 'BQCizZeUtI5hNv4WF2ZDUWzKsN6S1B3MoeVTNlWcEiNFfSyWCealo_BLq4RBxpl5KDIHBdNtmrpjiyvPpo0c3rU5wt2E2wwM_U9vXougElX8tu7KuNejM4_t_m8mXlzDjpR2-imdWMudnC39ZRQxd4DtNyQ7lANhE4bYL2ayT4d7DBQsYwRJtUbzWbuwiEBa6TWzl7k3uLbclzLb5XOYBb6_-qS5RRDmheE5V9zWy8JiBnvMyNJ3FrL3day-nNCz8rvNKnY5lffDMuk';
 
 const api = axios.create({
   baseURL: 'https://api.spotify.com/v1',
